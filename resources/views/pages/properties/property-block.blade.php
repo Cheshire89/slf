08d11a0f372c151c52ebb3e4b8d3eae50b91edc7
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2019-07-18 10:26:04 -0600

    update property formmating from get to arr

--- resources/views/pages/properties/property-block.blade.php
+++ resources/views/pages/properties/property-block.blade.php
@@ -1,33 +1,31 @@
 <div class="col-md-4 col-sm-6">
     <div class="at-property-item at-col-default-mar animated fadeInUpShort slow">
         <div class="at-property-img">
-            @if($property->get('MainImage'))
-                <img src="{{$property->get('MainImage')}}" alt="">
+            @if($property['MainImage'])
+                <img src="{{ $property['MainImage'] }}" alt="">
             @else
                 <img src="images/property/no_image.jpg" alt="">
             @endif
             <div class="at-property-overlayer"></div>
-        <a class="btn btn-default at-property-btn" href="/property/{{$property->get('MLSNumber')}}" role="button">View Details</a>
-            <h4>{{$property->get('TransactionType')}}</h4>
-        <h5>${{number_format($property->get('ListPrice'))}}</h5>
+        <a class="btn btn-default at-property-btn" href="/property/{{ $property['MLSNumber'] }}" role="button">View Details</a>
+            <h4>{{ $property['TransactionType'] }}</h4>
+        <h5>${{ $property['ListPrice'] }}</h5>
         </div>
         <div class="at-property-dis">
             <ul>
-                <li><i class="fa fa-object-group" aria-hidden="true"></i> {{number_format($property->get('SqftTotal'))}} sq ft</li>
-            <li><i class="fa fa-bed" aria-hidden="true"></i> {{$property->get('BedsTotal')}}</li>
-            <li><i class="fa fa-bath" aria-hidden="true"></i> {{$property->get('BathsTotal')}}</li>
+                <li><i class="fa fa-object-group" aria-hidden="true"></i> {{ $property['SqftTotal'] }} sq ft</li>
+            <li><i class="fa fa-bed" aria-hidden="true"></i> {{ $property['BedsTotal'] }}</li>
+            <li><i class="fa fa-bath" aria-hidden="true"></i> {{ $property['BathsTotal'] }}</li>
             </ul>
         </div>
         <div class="at-property-location">
-        <h4><i class="fa fa-home" aria-hidden="true"></i>
-            <a href="properties-details.html">
-                @if($property->get('BuildingName'))
-                    {{ $property->get('BuildingName') }}
-                @else
-                    {{ $property->get('PropertyType') }}
-                @endif
-            </a></h4>
-            <p><i class="fa fa-map-marker" aria-hidden="true"></i> {{$property->get('CompleteAddress')}}</p>
+            <h4>
+                <i class="fa fa-home" aria-hidden="true"></i>
+                <a href="properties-details.html">
+                    {{ $property['MainTitle'] }}
+                </a>
+            </h4>
+            <p><i class="fa fa-map-marker" aria-hidden="true"></i> {{ $property['CompleteAddress'] }}</p>
         </div>
     </div>
 </div>
